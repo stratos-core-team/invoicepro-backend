ec3fbce3dc9785d5d404a275335ab0ad2a49edc9
mlindokoj <johnmlindoko748@gmail.com>
2026-08-12 18:35:53 +0300

    Modify email verification token column size

--- database/migrations/005_email_verification.sql
+++ database/migrations/005_email_verification.sql
@@ -1,7 +1,8 @@
 ALTER TABLE users
-ADD COLUMN email_verified_at DATETIME NULL AFTER email,
-ADD COLUMN email_verification_token VARCHAR(128) NULL AFTER email_verified_at,
-ADD COLUMN email_verification_expires_at DATETIME NULL AFTER email_verification_token;
+ADD COLUMN email_verification_token VARCHAR(64) NULL
+    AFTER email_verified_at,
+ADD COLUMN email_verification_expires_at DATETIME NULL
+    AFTER email_verification_token;
 
 CREATE UNIQUE INDEX uq_users_email_verification_token
 ON users(email_verification_token);
